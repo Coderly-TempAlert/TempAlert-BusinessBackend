8cb4043c7a9af6a727e8e808cfceaf5b61c514e4
FrancoCarrillo <u202013943@upc.edu.pe>
2023-05-01 00:47:13 -0500

    configuration secrets

--- API/obj/Debug/net6.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("befe4031-0399-40c4-9cbc-fb0c87cb4ae5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
